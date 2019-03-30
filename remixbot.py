0a283c6d3983747d37db8af9fba8a55dc9d06745
uh_hey <uh_hey@localhost.localdomain>
2019-03-30 20:02:38 +0100

    cleaned up the code

--- remixbot.py
+++ remixbot.py
@@ -1,7 +1,10 @@
+import time
+import os
+import random
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram import ParseMode
-import time, os, random
+from telegram.utils.helpers import escape_markdown
 from mwt import MWT
 from slap_msgs import SLAP_TEMPLATES, ITEMS, THROW, HIT
 
@@ -15,6 +18,7 @@ GROUP_ID = os.environ.get("GROUP_ID")
 URL = os.environ.get("URL")
 
 updater = Updater(TOKEN)
+dispatcher = updater.dispatcher
 
 if URL:
     PORT = os.environ.get("PORT")
@@ -28,7 +32,7 @@ watchlist = []
 
 @MWT(timeout=3600)
 def get_admin_ids(bot, chat_id):
-    """Returns a list of admin IDs for a given chat. Results are cached for 1 hour."""
+    # Returns a list of admin IDs for a given chat. Results are cached for 1 hour.
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
 
 @run_async
@@ -38,39 +42,41 @@ def check(bot, update):
     message = update.effective_message
     chat = update.effective_chat
     
-    if (user.id in get_admin_ids(bot, chat.id)):
+    guide = "https://gitlab.com/uh_hey/telegram-remix-bot/wikis/How-to-set-a-username"
+    
+    if user.id in get_admin_ids(bot, chat.id):
         print(user.full_name + " is an admin.")
     else:
         
-        if (user.username == None):
+        if user.username is None:
             
-            if (user.id in watchlist):
+            if user.id in watchlist:
                 message.delete()
                 print("Deleted message from " + user.full_name)
             
             else:
                 print(user.full_name + " has no username. Waiting...")
                 watchlist.append(user.id)
-                msg_id = chat.send_message(user.full_name + ", please [get a username](https://gitlab.com/uh_hey/telegram-remix-bot/wikis/How-to-set-a-username) in 2 minutes or you will be kicked.", ParseMode.MARKDOWN)["message_id"]
+                msg_id = chat.send_message("{}, please [get a username]({}) in 2 minutes or you will be kicked.".format(escape_markdown(user.full_name), guide), ParseMode.MARKDOWN)["message_id"]
                 message.delete()
                 
                 time.sleep(120)
                 
                 bot.delete_message(chat.id, msg_id)
 
-                if (chat.get_member(user.id)["user"]["username"] == None):
+                if chat.get_member(user.id)["user"]["username"] is None:
                     
                     chat.kick_member(user.id)
                     watchlist.remove(user.id)
                     chat.unban_member(user.id)
                     print(user.full_name + " has been kicked.")
-                    msg_id = chat.send_message(user.full_name + " has been kicked.")["message_id"]
+                    msg_id = chat.send_message(escape_markdown(user.full_name) + " has been kicked.")["message_id"]
                     time.sleep(120)
                     bot.delete_message(chat.id, msg_id)
                 
                 else:
                     watchlist.remove(user.id)
-                    print(update.effective_user.full_name + " now has a username.")
+                    print(user.full_name + " now has a username.")
 
 @run_async
 def slap(bot, update):
@@ -79,13 +85,13 @@ def slap(bot, update):
     message = update.effective_message
     chat = update.effective_chat
     
-    if ((message.text).startswith("#slap")):
+    if (message.text).startswith("#slap"):
         
-        if (user.username != None):
+        if user.username is not None:
             
-            if (message.reply_to_message != None):
+            if message.reply_to_message is not None:
                 
-                if (message.reply_to_message.from_user.id == bot.id):
+                if message.reply_to_message.from_user.id == bot.id:
                     message.reply_text("Nah.")
                     return
                 
@@ -94,7 +100,7 @@ def slap(bot, update):
                 reply_msg = message.reply_to_message.message_id
             else:
                 
-                user2 = (message.text).split()
+                user2 = message.text.split()
                 
                 try:
                     user2[1]
@@ -102,20 +108,22 @@ def slap(bot, update):
                     message.reply_text("Reply to a message or type a username to use this command!")
                     return
                 
-                if (user2[1].startswith("@")):
+                if user2[1].startswith("@"):
                     user2 = user2[1]
                 else:
                     user2 = "@" + user2[1]
                 
-                if ((user2 == "@admin") | ("/" in user2)):
+                if user2 == "@admin" or "/" in user2:
                     message.delete()
                     return
-                if (user2[1:] == bot.username):
+                if user2[1:] == bot.username:
                     message.reply_text("Nah.")
                     return
-                if ((len(user2) < 6) | (len(user2) > 33)):
+                if len(user2) < 6 or len(user2) > 33:
                     message.reply_text("That user doesn't exist! This command only works with usernames and replies.")
                     return
+                
+                user2 = escape_markdown(user2)
                 
                 reply_msg = message.message_id
             
@@ -129,11 +137,14 @@ def slap(bot, update):
             chat.send_message(temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw), ParseMode.MARKDOWN, reply_to_message_id=reply_msg)
 
 if GROUP_ID:
-    updater.dispatcher.add_handler(MessageHandler((Filters.chat(int(GROUP_ID)) & Filters.group), check))
-    updater.dispatcher.add_handler(MessageHandler((Filters.chat(int(GROUP_ID)) & Filters.group & Filters.text), slap), -1)
+    check_handler = MessageHandler(Filters.chat(int(GROUP_ID)) and Filters.group, check)
+    slap_handler = MessageHandler(Filters.chat(int(GROUP_ID)) and Filters.group & Filters.text, slap)
 else:
-    updater.dispatcher.add_handler(MessageHandler(Filters.group, check))
-    updater.dispatcher.add_handler(MessageHandler((Filters.group & Filters.text), slap), -1)
+    check_handler = MessageHandler(Filters.group, check)
+    slap_handler = MessageHandler(Filters.group and Filters.text, slap)
+    
+dispatcher.add_handler(check_handler)
+dispatcher.add_handler(slap_handler, 1)
 
 if not URL:
     updater.start_polling()
