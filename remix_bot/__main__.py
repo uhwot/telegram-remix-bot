8ad457b319d5c4fb94e0caf5fc854f6fd36d128d
uh_wot <3631986-uh_wot@users.noreply.gitlab.com>
2019-06-04 19:15:38 +0200

    some more logging won't hurt

--- remix_bot/__main__.py
+++ remix_bot/__main__.py
@@ -8,15 +8,24 @@ if URL:
                           port=int(PORT),
                           url_path=TOKEN)
     if CERT_PATH:
+        logging.info("Webhooks with HTTPS certificate enabled.")
         updater.bot.set_webhook(URL + TOKEN,
                                 certificate=open(CERT_PATH, "rb"))
+
     else:
+        logging.info("Webhooks without HTTPS certificate enabled.")
         updater.bot.set_webhook(URL + TOKEN)
+
+else:
+    logging.info("Webhooks disabled, using long polling instead.")
 
 import remix_bot.username_check
 import remix_bot.fun
 if DB_URL:
+    logging.info("Database enabled.")
     import remix_bot.database
+else:
+    logging.info("Database disabled.")
 
 
 if not URL:
