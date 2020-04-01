83dc1467b4be635c7f3d27e0a895bfa622705087
uh_wot <3631986-uh_wot@users.noreply.gitlab.com>
2020-04-01 13:52:48 +0200

    haha april fools joek

--- remix_bot/modules/fun.py
+++ remix_bot/modules/fun.py
@@ -14,81 +14,14 @@ from ..slap_msgs import *
 @group_id
 @username
 def slap(update: Update, context: CallbackContext):
-    user = update.effective_user
-    message = update.effective_message
-    chat = update.effective_chat
-    bot = context.bot
-
-    user1 = f"[{user.full_name}](tg://user?id={user.id})"
-    user2 = None
-    self = False
-    basic = False
-
-    if message.reply_to_message:
-
-        if message.reply_to_message.from_user.id == bot.id:
-            message.reply_text("Nah.")
-            return
-
-        if user.id == message.reply_to_message.from_user.id:
-            self = True
-        else:
-            user2 = f"[{message.reply_to_message.from_user.full_name}](tg://user?id={message.reply_to_message.from_user.id})"
-
-        reply_msg = message.reply_to_message.message_id
-    else:
-
-        try:
-            user2 = message.parse_entity(message.entities[1])[1:]
-        except IndexError:
-            basic = True
-        else:
-
-            temp = user2.lower()
-
-            if temp == "admin" or "/" in temp:
-                delete(message)
-                return
-            if temp == bot.username.lower():
-                message.reply_text("Nah.")
-                return
-            if temp == user.username.lower():
-                self = True
-            else:
-
-                if DB_URL:
-                    try:
-                        user2 = f"[{get_name(user2)}](tg://user?id={get_id(user2)})"
-                    except KeyError:
-                        user2 = "@" + escape_markdown(user2)
-                else:
-                    user2 = "@" + escape_markdown(user2)
-
-        reply_msg = message.message_id
-
-    if self:
-        temp = random.choice(SLAP_SELF)
-    elif basic:
-        temp = random.choice(SLAP_BASIC)
-    else:
-        temp = random.choice(SLAP_TEMPLATES)
-
-    item = random.choice(ITEMS)
-    hit = random.choice(HIT)
-    throw = random.choice(THROW)
-
-    chat.send_message(
-        temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw),
-        ParseMode.MARKDOWN,
-        reply_to_message_id=reply_msg,
-    )
+    update.effective_message.reply_text("i won't slap for u, ur too gay")
 
 
 @run_async
 @group_id
 @username
 def runs(update: Update, _):
-    update.effective_message.reply_text(random.choice(RUN_STRINGS))
+    update.effective_message.reply_text("bruh y u running")
 
 
 @run_async
